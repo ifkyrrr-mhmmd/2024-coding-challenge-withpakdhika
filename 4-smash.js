5317ab32a5a323d9a9070677546c4ce1c744c638
unknown <mhmmdrifky2330@gmail.com>
2024-02-11 10:08:09 +0700

    4/366

---
+++ 4-smash.js
@@ -0,0 +1,25 @@
+// WPU Coding Challenge With Pak Dhika
+// 4/366
+
+// function smash(words) {
+//   return words.join(" ");
+// }
+
+// const smash = (words) => words.join(" ");
+
+function smash(words) {
+  let hasil = "";
+  for (let i = 0; i < words.length; i++) {
+    hasil += words[i];
+    if (i != words.length - 1) hasil += " ";
+  }
+  return hasil;
+}
+
+// buatkan aku sebuah fungsi
+// buatkan aku sebuah variabel kosong
+// buatkan aku sebuah pengulangan, yang dimana array akan ditambahkan sesuai dengan panjang array
+// buat pengkondisian, dimana jika i tidak sama dengan panjang array dikurang satu, maka array akan dberi spasi (untuk array terakhir tidak diberi)
+// kembalikan hasil awal
+
+console.log(smash(["hello", "amazing", "world"]));
