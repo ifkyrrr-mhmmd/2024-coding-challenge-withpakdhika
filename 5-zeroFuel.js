57983952461394332ff9b4effedd93080b7577b3
unknown <mhmmdrifky2330@gmail.com>
2024-02-11 15:03:30 +0700

    Day 5/366

--- 5-zeroFuel.js
+++ 5-zeroFuel.js
@@ -1,8 +1,16 @@
 // WPU Coding Challenge With Pak Dhika
 // 5/366
 
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-  return distanceToPump / mpg <= fuelLeft ? true : false;
-};
+// Question!!
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+// Function should return true if it is possible and false if not.
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   return distanceToPump / mpg <= fuelLeft;
+// };
 // jika jarak ke pom bensin dibagi miles per gallon lebih kecil daripadanya, maka hasilnya akan true
-console.log(zeroFuel(50, 25, 2));
+const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
+  fuelLeft * mpg >= distanceToPump;
+
+console.log(zeroFuel(75, 25, 3));
