396c3b6222defc74730858d25b61bc0c425aa910
unknown <mhmmdrifky2330@gmail.com>
2024-02-12 17:30:28 +0700

    Day 6/366

--- 6-DNAtoRNA.js
+++ 6-DNAtoRNA.js
@@ -1,9 +1,14 @@
-function DNAtoRNA(dna) {
-  let rna = "";
-  for (i = 0; i < dna.length; i++) {
-    dna[i] === "T" ? (rna += "U") : (rna += dna[i]);
-  }
-  return rna;
-}
+// function DNAtoRNA(dna) {
+//   let rna = "";
+//   for (i = 0; i < dna.length; i++) {
+//     dna[i] === "T" ? (rna += "U") : (rna += dna[i]);
+//   }
+//   return rna;
+// }
 
+const DNAtoRNA = (dna) =>
+  dna
+    .split("")
+    .map((el) => (el === "T" ? "U" : el))
+    .join("");
 console.log(DNAtoRNA("GCATTTT"));
