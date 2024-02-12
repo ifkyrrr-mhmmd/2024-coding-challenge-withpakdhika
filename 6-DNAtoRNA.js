2489faf2ba4c8806b27ce6118cb6513d232543a0
unknown <mhmmdrifky2330@gmail.com>
2024-02-12 17:44:14 +0700

    Day 6/366

--- 6-DNAtoRNA.js
+++ 6-DNAtoRNA.js
@@ -9,10 +9,17 @@
 //   return rna;
 // }
 
-const DNAtoRNA = (dna) =>
-  dna
-    .split("")
-    .map((el) => (el === "T" ? "U" : el))
-    .join("");
+// const DNAtoRNA = (dna) =>
+//   dna
+//     .split("")
+//     .map((el) => (el === "T" ? "U" : el))
+//     .join("");
+
+const DNAtoRNA = (dna) => dna.split("T").join("U");
 
 console.log(DNAtoRNA("GCATTTT"));
+
+// js help aku buat variabel dengan nama DNA,
+// lalu lakukan perintah di dalamnya javascriptt tolong split dulu elemennya,
+// kemudian satukan kembali menggunakan fungsi map ( yang akan berbentuk sebuah string), lalu lakukan pengkondisian di dalamnya,
+// lalu jika sudah, gabungkan kembali stringnya dengan menggunakan fungsi join( disertai dengan tanda petik(kosong))
