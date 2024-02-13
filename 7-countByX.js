8700f5064cb1addc6b1e10e28c160554c2a6e97e
unknown <mhmmdrifky2330@gmail.com>
2024-02-13 21:33:33 +0700

    Day 7/366

--- 7-countByX.js
+++ 7-countByX.js
@@ -1,12 +1,16 @@
 // WPU Coding Challenge With Pak Dhika
 // 7/366
 
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 1; i <= n; i++) {
+//     z.push(i * x);
+//   }
+//   return z;
+// }
+
 function countBy(x, n) {
-  let z = [];
-  for (let i = 1; i <= n; i++) {
-    z.push(i * x);
-  }
-  return z;
+  return [...Array(n)].map((el, i) => (i + 1) * x);
 }
 
-console.log(countBy(2, 8));
+console.log(countBy(2, 5));
