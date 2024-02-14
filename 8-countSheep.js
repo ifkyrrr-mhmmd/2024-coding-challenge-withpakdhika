eb352b9bffd71352ea7e3cad2eec0e6a1673d0a3
unknown <mhmmdrifky2330@gmail.com>
2024-02-14 08:30:25 +0700

    Day 8/366

--- 8-countSheep.js
+++ 8-countSheep.js
@@ -1,7 +1,19 @@
+// WPU Coding Challenge With Pak Dhika
+// 8/366
+
+// var countSheep = function (num) {
+//   let hasil = "";
+//   for (i = 1; i <= num; i++) {
+//     hasil += i + " sheep...";
+//   }
+
+//   return hasil;
+// };
+
 var countSheep = function (num) {
   let hasil = "";
   for (i = 1; i <= num; i++) {
-    hasil += i + " sheep...";
+    hasil += `${i} sheep...`;
   }
 
   return hasil;
