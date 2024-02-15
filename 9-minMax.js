16392e900d353d6d27ff18a430477effefaae6f9
unknown <mhmmdrifky2330@gmail.com>
2024-02-15 11:27:48 +0700

    Day 9/366

---
+++ 9-minMax.js
@@ -0,0 +1,34 @@
+// WPU Coding Challenge With Pak Dhika
+// 9/366
+
+// Cara Barbar
+// var min = function (list) {
+//   let nilaiMin = list[0];
+//   for (let i = 1; i < list.length; i++) {
+//     if (nilaiMin > list[i]) {
+//       nilaiMin = list[i];
+//     }
+//   }
+//   return nilaiMin;
+// };
+
+// var max = function (list) {
+//   let nilaiMax = list[0];
+//   for (let i = 1; i < list.length; i++) {
+//     if (nilaiMax < list[i]) {
+//       nilaiMax = list[i];
+//     }
+//   }
+//   return nilaiMax;
+// };
+
+// menggunakan fungsi sort
+// const min = (list) => list.sort((a, b) => a - b)[0];
+// const max = (list) => list.sort((a, b) => b - a)[0];
+
+// menggunakan fungsi Math
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+console.log(max([-52, 56, 30, 29, -54, 0, -110]));
