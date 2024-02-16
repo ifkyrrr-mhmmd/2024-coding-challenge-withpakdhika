9afb1b5098330dcaea1493c8f324a8cddee07683
unknown <mhmmdrifky2330@gmail.com>
2024-02-16 15:57:32 +0700

    Day 10/366

--- 10-stringToArray.js
+++ 10-stringToArray.js
@@ -1,16 +1,9 @@
 // WPU Coding Challenge With Pak Dhika
 // 10/366
 
-// cara barbar menggunakan looping
-function stringToArray(string) {
-  let hasil = [""];
-
-  return (hasil += string);
-}
-
 // menggunakan fungsi split = untuk memisahkan kata menjadi string
-// function stringToArray(string) {
-//   return string.split(" ");
-// }
+function stringToArray(string) {
+  return string.split(" ");
+}
 
 console.log(stringToArray("Robin Singh"));
