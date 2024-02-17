dcb22cdf1ac74c26bd6ef17faf13044b4051f752
unknown <mhmmdrifky2330@gmail.com>
2024-02-17 23:56:57 +0700

    Day 9/366

--- 9-minMax.js
+++ 9-minMax.js
@@ -31,4 +31,4 @@ const min = (list) => Math.min(...list);
 const max = (list) => Math.max(...list);
 
 console.log(min([-52, 56, 30, 29, -54, 0, -110]));
-console.log(max([-52, 56, 30, 29, -54, 0, -110]));
+console.log(max([-53, 57, 30, 29, -54, 0, -110]));
