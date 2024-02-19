e18eb7c4199d437827e67032bb14aa74e046f8c4
unknown <mhmmdrifky2330@gmail.com>
2024-02-19 22:03:18 +0700

    Day 11/366

--- 11-rps.js
+++ 11-rps.js
@@ -1,16 +1,26 @@
+// const rps = (p1, p2) => {
+//   if (p1 === p2) {
+//     return "Draw!";
+//   }
+//   if (p1 === "rock" && p2 === "scissors") {
+//     return "Player 1 won!";
+//   } else if (p1 === "scissors" && p2 === "paper") {
+//     return "Player 1 won!";
+//   } else if (p1 === "paper" && p2 === "rock") {
+//     return "Player 1 won!";
+//   } else {
+//     return "Player 2 won!";
+//   }
+// };
+
 const rps = (p1, p2) => {
-  if (p1 === p2) {
-    return "Draw!";
-  }
-  if (p1 === "rock" && p2 === "scissors") {
-    return "Player 1 won!";
-  } else if (p1 === "scissors" && p2 === "paper") {
-    return "Player 1 won!";
-  } else if (p1 === "paper" && p2 === "rock") {
-    return "Player 1 won!";
-  } else {
-    return "Player 2 won!";
-  }
+  return p1 === p2
+    ? "Draw!"
+    : (p1 === "rock" && p2 === "scissors") ||
+      (p1 === "scissors" && p2 === "paper") ||
+      (p1 === "paper" && p2 === "rock")
+    ? "Player 1 won!"
+    : "Player 2 won!";
 };
 
-console.log(rps("rock", "scissors"));
+console.log(rps("paper", "paper"));
