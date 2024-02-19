e6d31f2f3bd0a9d20e406edfee01f5bb7d8a4fc1
unknown <mhmmdrifky2330@gmail.com>
2024-02-19 22:05:02 +0700

    Day 11/366

--- 11-rps.js
+++ 11-rps.js
@@ -13,14 +13,23 @@
 //   }
 // };
 
-const rps = (p1, p2) => {
-  return p1 === p2
+// const rps = (p1, p2) => {
+//   return p1 === p2
+//     ? "Draw!"
+//     : (p1 === "rock" && p2 === "scissors") ||
+//       (p1 === "scissors" && p2 === "paper") ||
+//       (p1 === "paper" && p2 === "rock")
+//     ? "Player 1 won!"
+//     : "Player 2 won!";
+// };
+
+const rps = (p1, p2) =>
+  p1 === p2
     ? "Draw!"
     : (p1 === "rock" && p2 === "scissors") ||
       (p1 === "scissors" && p2 === "paper") ||
       (p1 === "paper" && p2 === "rock")
     ? "Player 1 won!"
     : "Player 2 won!";
-};
 
 console.log(rps("paper", "paper"));
